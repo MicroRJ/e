3996e251a7402e5fd0062868125c29ea253a4ab2
Dayan <36505543+MicroRJ@users.noreply.github.com>
2023-07-29 17:28:11 +0000

    removed some vestigial functions

--- ewidget.h
+++ ewidget.h
@@ -179,7 +179,8 @@ rect_padd(erect_t rect, int xpadd, int ypadd)
       rect.y1 - (ypadd >> 1));
 }
 
-erect_t text_rect(int x0, int y0, int height, const char *string)
+erect_t
+text_rect(int x0, int y0, int height, const char *string)
 {
   erect_t result =
     rect_by_size(x0,y0,rxdraw_text_length(height,string),height);
@@ -187,7 +188,8 @@ erect_t text_rect(int x0, int y0, int height, const char *string)
   return result;
 }
 
-void draw_rect(erect_t rect, rxcolor_t color)
+void
+draw_rect(erect_t rect, rxcolor_t color)
 {
   rxdraw_rect(color,
     rect.x0, rect.y0,
@@ -195,154 +197,15 @@ void draw_rect(erect_t rect, rxcolor_t color)
     (rect.y1 - rect.y0));
 }
 
-void draw_text(erect_t rect, const char *text, int size, rxcolor_t color)
+void
+draw_text(erect_t rect, const char *text, int size, rxcolor_t color)
 {
   rxdraw_text_ex(color,rect.x0,rect.y0,size,strlen(text),text);
 }
 
-void set_clip_rect(erect_t rect)
+void
+set_clip_rect(erect_t rect)
 {
   rxclip(
     rect.x0,rect.y0,rect.x1,rect.y1);
-}
-
-int button_widget(erect_t rect, const char *string)
-{
-  set_clip_rect(rect);
-
-  draw_rect(rect,BACKGROUND_COLOR);
-
-  erect_t the_text_rect =
-    rect_center(rect,text_rect(0,0,32,string));
-
-  draw_text(the_text_rect,string,32,
-    rect_in_xy(rect,rx.xcursor,rx.ycursor) ?
-      RX_COLOR_GREEN : FOREGROUND_COLOR);
-
-  int result = is_click_leave_rect(rect);
-
-  set_clip_rect(RECT_HUGE);
-  return result;
-}
-
-int checkbox_widget(erect_t rect, char const *label, int *is_on)
-{
-  set_clip_rect(rect);
-
-  draw_rect(rect,BACKGROUND_COLOR);
-
-  int is_over = rect_in_xy(rect,rx.xcursor,rx.ycursor);
-
-
-  erect_t r;
-  rxcolor_t color;
-  erect_t q;
-
-  r = rect_padd(rect,4,4);
-
-  /* the indicator */
-  { color = *is_on ? RX_COLOR_GREEN : RX_COLOR_RED;
-
-    char const *ts = *is_on?"[yes]":"[nop]";
-
-    q = text_rect(0,0,32,ts);
-    q = rect_center(rect_cut(&r,RECT_kRGT,q.x1-q.x0),q);
-    draw_text(q,ts,32,color);
-  }
-  /* the label */
-  { color = is_over ? RX_COLOR_GREEN : FOREGROUND_COLOR;
-
-    q = text_rect(0,0,32,label);
-    q = rect_cut(&r,RECT_kLFT,q.x1-q.x0);
-    draw_text(q,label,32,color);
-  }
-
-  if(is_click_leave_rect(rect))
-  {
-    *is_on = !*is_on;
-  }
-
-  set_clip_rect(RECT_HUGE);
-
-  return *is_on;
-}
-
-int dropdown_widget(erect_t rect, const char *string, int *is_on)
-{
-  set_clip_rect(rect);
-
-  draw_rect(rect,BACKGROUND_COLOR);
-
-  int is_over = rect_in_xy(rect,rx.xcursor,rx.ycursor);
-
-  rxcolor_t color = is_over ? RX_COLOR_GREEN :
-    (*is_on) ? RX_COLOR_YELLOW : FOREGROUND_COLOR;
-
-  erect_t the_btn_prnt_rect = rect;
-  char const *the_btn_text = *is_on?"[-]":"[+]";
-
-  erect_t the_btn_text_rect = text_rect(0,0,32,the_btn_text);
-  the_btn_text_rect = rect_center(
-    rect_cut(&the_btn_prnt_rect,RECT_kLFT,the_btn_text_rect.x1-the_btn_text_rect.x0),the_btn_text_rect);
-
-  draw_text(the_btn_text_rect,the_btn_text,32,color);
-
-  erect_t the_text_rect = rect_center(rect,text_rect(0,0,32,string));
-  draw_text(the_text_rect,string,32,color);
-
-  if(is_click_leave_rect(rect))
-  {
-    *is_on = !*is_on;
-  }
-
-  set_clip_rect(RECT_HUGE);
-
-  return *is_on;
-}
-
-int list_item_widget(erect_t rect, rxcolor_t color, const char *string)
-{
-  set_clip_rect(rect);
-
-  erect_t the_text_rect = rect;
-
-  draw_rect(rect,BACKGROUND_COLOR);
-
-  draw_text(the_text_rect,string,32,
-    rect_in_xy(rect,rx.xcursor,rx.ycursor)?RX_COLOR_GREEN:color);
-
-  int result = is_click_leave_rect(rect);
-
-  set_clip_rect(RECT_HUGE);
-  return result;
-}
-
-int slider_widget(erect_t rect, const char *label, double min, double max, double *value)
-{
-  draw_rect(rect,BACKGROUND_COLOR);
-
-  erect_t the_inner_rect = rect_padd(rect,4,4);
-
-  int the_thumb_offset =
-    rxremix(
-      rxclamp(*value,min,max),min,max,0,
-        the_inner_rect.x1 - the_inner_rect.x0 - 0x10);
-
-  erect_t the_thumb_rect =
-    rect_by_xyxy(
-      the_inner_rect.x0 + the_thumb_offset + 0x00, the_inner_rect.y0 + 0x00,
-      the_inner_rect.x0 + the_thumb_offset + 0x10, the_inner_rect.y1 + 0x00 );
-
-  int is_active = rxtstbtn(0) && rect_in_xy(rect,rx.xcursor,rx.ycursor);
-
-  if(is_active)
-  {
-    *value = rxclamp(rxremix(rx.xcursor,
-      the_inner_rect.x0,the_inner_rect.x1,min,max),min,max);
-  }
-
-
-  draw_rect(the_thumb_rect,RX_RGBA_UNORM(0x32,0x32,0x32,0xFF));
-
-  return is_active;
 }
